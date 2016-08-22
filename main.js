2b150a8be9bae867b0878e1f0c91262570c8e033
Megafatsquirrel <megafatsquirrel@gmail.com>
2016-08-21 19:35:02 -0700

    Player can now attack

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ document.addEventListener('DOMContentLoaded', function(e) {
 	var GAME_PIECE_HIGHLIGHT_COLOR_NPC = '#ffedb6';
 	var GAME_PIECE_DEFAULT_COLOR_PC = '#d40dcb';
 	var GAME_PIECE_HIGHLIGHT_COLOR_PC = '#f7abf3';
-	var HIGHLIGHT_COLOR = '#7e98e8';
-	var HIGHLIGHT_COLOR_ATTACK = '#ef0f0f';
+	var HIGHLIGHT_COLOR_MOVE = 'rgba(128, 145, 241, 0.6)';
+	var HIGHLIGHT_COLOR_ATTACK = 'rgba(251, 34, 34, 0.6)';
 
 	var gameRound = 0;
 
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 			}
 		}
 	}
-
+	var gameMapOverlay = gameMap;
 	// setup game objects
 	var weapon1 = { name: 'dagger', damage: 2 };
 	var armor1 = { name: 'rags', defense: 1 };
@@ -75,6 +75,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 			height: height,
 			color: color,
 			hasMoved: false,
+			hasAttacked: false,
 			id: id,
 			location: location, //1|2
 			name: name,
@@ -84,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 			weapon: weapon,
 			armor: armor,
 			attack: weapon.damage * 1, // TODO Add a dynamic mod for game piece type
-			attackRange: 1,
+			attackRange: 2,
 			defense: armor.defense * 1, // TODO Add a dynamic mod for game piece type
 			type: isNPC ? 'NPC' : 'PC'
 		};
@@ -100,7 +101,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 			height: height,
 			color: color,
 			gamePieceId: '',
-			isHighlight: false,
+			isHighlightMove: false,
 			isHighlightAttack: false,
 			occupied: false,
 			occupiedType: '',
@@ -131,6 +132,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 	var clientYoutput = document.getElementById('clientYoutput');
 	var clickXoutput = document.getElementById('clickXoutput');
 	var clickYoutput = document.getElementById('clickYoutput');
+
 	function createInputEvents() {
 		document.addEventListener("mousemove", function(e) {
 			clientXoutput.innerHTML = e.clientX;
@@ -149,15 +151,29 @@ document.addEventListener('DOMContentLoaded', function(e) {
 				var indexY = parseInt(clickY / TILE_HEIGHT);
 				if (indexX >= 0 && indexY >= 0 && indexX <= MAP_WIDTH && indexY <= MAP_HEIGHT) {
 					var mapTile = gameMap[indexX][indexY];
-					if (mapTile !== undefined && mapTile.occupied) {
+					if (mapTile !== undefined && mapTile.occupied && !mapTile.isHighlightAttack) {
 						gameObjects.forEach(function(item) {
 							if (item.id === mapTile.gamePieceId) {
 								selectGamePiece(item);
 							}
 						});
 					}else if (currentSelectedTile !== null){ // clicked outside of the occupied square
+						// attack piece
+						if (!currentSelectedTile.hasAttacked && mapTile.occupied && mapTile.isHighlightAttack && currentSelectedTile.type === 'PC'){
+							for (var i = 0; i < gameObjects.length; i++) {
+								var enemy = gameObjects[i];
+								if (mapTile.gamePieceId === enemy.id){
+									if (enemy.type === 'NPC'){
+										enemy.currentHP -= currentSelectedTile.attack;
+										currentSelectedTile.hasMoved = true;
+										currentSelectedTile.hasAttacked = true;
+									}
+								}
+							}
+						}
+
 						// move piece
-						if (!currentSelectedTile.hasMoved && !mapTile.occupied && mapTile.isHighlight && currentSelectedTile.type === 'PC'){
+						if (!currentSelectedTile.hasMoved && !mapTile.occupied && mapTile.isHighlightMove && currentSelectedTile.type === 'PC'){
 							for (var i = 0; i < gameObjects.length; i++) {
 								if (currentSelectedTile.id === gameObjects[i].id){
 									var currentGamePiece = gameObjects[i];
@@ -190,12 +206,14 @@ document.addEventListener('DOMContentLoaded', function(e) {
 					}
 				});
 			}
-			
 		});
 
 		endTurnBtn.addEventListener('click', function(e){
+			// TODO add enemy logic here
+
 			for (var i = 0; i < gameObjects.length; i++) {
 				gameObjects[i].hasMoved = false;
+				gameObjects[i].hasAttacked = false;
 			}
 			gameRound++;
 			currentRound.innerHTML = gameRound;
@@ -211,7 +229,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 		gamePiece.color = gamePiece.type === 'PC' ? GAME_PIECE_HIGHLIGHT_COLOR_PC : GAME_PIECE_HIGHLIGHT_COLOR_NPC;
 
 		gamePieceName.innerHTML = gamePiece.name;
-		gamePieceHealth.innerHTML = gamePiece.health + " / " + gamePiece.currentHP;
+		gamePieceHealth.innerHTML = gamePiece.currentHP + " / " + gamePiece.health;
 		gamePieceMovement.innerHTML = gamePiece.movement;
 		gamePieceAttack.innerHTML = gamePiece.attack;
 		gamePieceDefense.innerHTML = gamePiece.defense;
@@ -222,89 +240,92 @@ document.addEventListener('DOMContentLoaded', function(e) {
 		gamePieceInfoDisplay.style.display = 'block';
 		gamePieceInfoDisplayDefault.style.display = 'none';
 
-		
 		if (gamePiece.type === 'PC'){
 			if (!gamePiece.hasMoved)
-				highlightMovementArea(gamePiece);
+				highlightMoveAttackArea(gamePiece, gamePiece.movement, false);
+			if (!gamePiece.hasAttacked)
+			highlightMoveAttackArea(gamePiece, gamePiece.attackRange, true);
 		}
 	}
 
-	function highlightMovementArea(gamePiece) {
+	function highlightMoveAttackArea(gamePiece, actionLength, isAttack) {
 		var rightTile;
 		var leftTile;
 		var topTile;
 		var downTile;
-		for(var i = 1; i <= gamePiece.movement; i++){
+		for(var i = 1; i <= actionLength; i++){
 			// right
 			if (gamePiece.location.x + i < MAP_WIDTH){
 				rightTile = gameMap[gamePiece.location.x + i][gamePiece.location.y];
-				checkTileMoveAttack(rightTile, gamePiece);
-				selectSideTiles(gamePiece, gamePiece.movement - i, gamePiece.location.x + i, false);
+				checkTileMoveAttack(rightTile, gamePiece, isAttack);
+				selectSideTiles(gamePiece, actionLength - i, gamePiece.location.x + i, false, isAttack);
 			}
 			// left
 			if (gamePiece.location.x - i >= 0){
 				leftTile = gameMap[gamePiece.location.x - i][gamePiece.location.y];
-				checkTileMoveAttack(leftTile, gamePiece);
-				selectSideTiles(gamePiece, gamePiece.movement - i, gamePiece.location.x - i, true);
+				checkTileMoveAttack(leftTile, gamePiece, isAttack);
+				selectSideTiles(gamePiece, actionLength - i, gamePiece.location.x - i, true, isAttack);
 			}
 			// top
 			if (gamePiece.location.y - i >= 0){
 				topTile = gameMap[gamePiece.location.x][gamePiece.location.y - i];
-				checkTileMoveAttack(topTile, gamePiece);
-				selectDownTiles(gamePiece, gamePiece.movement - i, gamePiece.location.y - i, true);
+				checkTileMoveAttack(topTile, gamePiece, isAttack);
+				selectDownTiles(gamePiece, actionLength - i, gamePiece.location.y - i, true, isAttack);
 			}
 			// down
 			if (gamePiece.location.y + i < MAP_HEIGHT){
 				downTile = gameMap[gamePiece.location.x][gamePiece.location.y + i];
-				checkTileMoveAttack(downTile, gamePiece);
-				selectDownTiles(gamePiece, gamePiece.movement - i, gamePiece.location.y + i, false);
+				checkTileMoveAttack(downTile, gamePiece, isAttack);
+				selectDownTiles(gamePiece, actionLength - i, gamePiece.location.y + i, false, isAttack);
 			}
 		}
 	}
 
-	function selectSideTiles(gamePiece, movement, index, reverse){
+	function selectSideTiles(gamePiece, movement, index, reverse, isAttack){
 		for(var i = 1; i <= movement; i++){
 			if (!reverse){
 				if (gamePiece.location.y + i < MAP_HEIGHT){
 					var tile = gameMap[index][gamePiece.location.y + i];
-					checkTileMoveAttack(tile, gamePiece);
+					checkTileMoveAttack(tile, gamePiece, isAttack);
 				}
 			}else{
 				if (gamePiece.location.y - i >= 0){
 					var tile = gameMap[index][gamePiece.location.y - i];
-					checkTileMoveAttack(tile, gamePiece);
+					checkTileMoveAttack(tile, gamePiece, isAttack);
 				}
 			}
 		}
 	}
 
-	function checkTileMoveAttack(tile, gamePiece) {
-		if (!tile.occupied){
-			tile.isHighlight = true;
-		}
-	}
-
-	function selectDownTiles(gamePiece, movement, index, reverse){
+	function selectDownTiles(gamePiece, movement, index, reverse, isAttack){
 		for(var i = 1; i <= movement; i++){
 			if (!reverse){
 				if (gamePiece.location.x - i >= 0){
 					var tile = gameMap[gamePiece.location.x - i][index];
-					checkTileMoveAttack(tile, gamePiece);
+					checkTileMoveAttack(tile, gamePiece, isAttack);
 				}
 			}else{
 				if (gamePiece.location.x + i < MAP_WIDTH){
 					var tile = gameMap[gamePiece.location.x + i][index];
-					checkTileMoveAttack(tile, gamePiece);
+					checkTileMoveAttack(tile, gamePiece, isAttack);
 				}
 			}
+		}
+	}
+
+	function checkTileMoveAttack(tile, gamePiece, isAttack) {
+		if (!tile.occupied && !isAttack){
+			tile.isHighlightMove = true;
+		}else{
+			tile.isHighlightAttack = true;
 		}
 	}
 
 	function deselectGameMapTiles() {
 		for (var i = 0; i < MAP_WIDTH; i++){
 			for (var j = 0; j < MAP_HEIGHT; j++){
-				if (gameMap[i][j].isHighlight || gameMap[i][j].isHighlightAttack) {
-					gameMap[i][j].isHighlight = false;
+				if (gameMap[i][j].isHighlightMove || gameMap[i][j].isHighlightAttack) {
+					gameMap[i][j].isHighlightMove = false;
 					gameMap[i][j].isHighlightAttack = false;
 				}
 			}
@@ -329,9 +350,8 @@ document.addEventListener('DOMContentLoaded', function(e) {
 		// draw map
 		for (var i = 0; i < MAP_WIDTH; i++){
 			for (var j = 0; j < MAP_HEIGHT; j++){
-				ctx.fillStyle = gameMap[i][j].isHighlight ? HIGHLIGHT_COLOR : gameMap[i][j].color;
-				if (gameMap[i][j].isHighlightAttack)
-					ctx.fillStyle = HIGHLIGHT_COLOR_ATTACK;
+				ctx.fillStyle = gameMap[i][j].color;
+				
 				ctx.fillRect(gameMap[i][j].left, 
 					gameMap[i][j].top, 
 					gameMap[i][j].width, 
@@ -357,18 +377,23 @@ document.addEventListener('DOMContentLoaded', function(e) {
 		// draw overlay
 		for (var i = 0; i < MAP_WIDTH; i++){
 			for (var j = 0; j < MAP_HEIGHT; j++){
-				if (gameMap[i][j].isHighlightAttack || gameMap[i][j].isHighlight){
-				ctx.fillStyle = gameMap[i][j].isHighlight ? HIGHLIGHT_COLOR : HIGHLIGHT_COLOR_ATTACK;
-				ctx.fillRect(gameMap[i][j].left, 
-					gameMap[i][j].top, 
-					gameMap[i][j].width, 
-					gameMap[i][j].height);
+				if (gameMap[i][j].isHighlightAttack || gameMap[i][j].isHighlightMove){
 
-				ctx.fillStyle = '#000000';
-				ctx.strokeRect(gameMap[i][j].left, 
-					gameMap[i][j].top, 
-					gameMap[i][j].width, 
-					gameMap[i][j].height);
+					if (gameMap[i][j].isHighlightMove)
+						ctx.fillStyle = HIGHLIGHT_COLOR_MOVE;
+					if (gameMap[i][j].isHighlightAttack)
+						ctx.fillStyle = HIGHLIGHT_COLOR_ATTACK;
+					
+					ctx.fillRect(gameMap[i][j].left, 
+						gameMap[i][j].top, 
+						gameMap[i][j].width, 
+						gameMap[i][j].height);
+
+					ctx.fillStyle = '#000000';
+					ctx.strokeRect(gameMap[i][j].left, 
+						gameMap[i][j].top, 
+						gameMap[i][j].width, 
+						gameMap[i][j].height);
 				}
 			}
 		}
